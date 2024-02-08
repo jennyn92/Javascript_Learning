6751a91bb04debfb9fb497c7b8e869851b66499e
jennyn92 <134844026+jennyn92@users.noreply.github.com>
2024-02-08 20:56:27 +0000

    arrays

--- session3.js
+++ session3.js
@@ -10,3 +10,71 @@ function getSum(num1,nume2){
   const ans =getSum(2,4)
   console.log(ans)
   
+
+// Arrays
+//Storing multiple elements
+//arrays syntax
+console.clear()
+
+//as variable
+const fruit1 = "apple"
+
+//as array
+const fruit = ["apple", "plum", "cherry"]
+
+//[indexing, or picking a particular item]
+console.log(fruit[0])
+
+
+//Defining and using objects
+
+
+//key:value pairs make up objects
+//title:"Invisible Women"
+const invisibleWomen = {
+    title: "Invisible Women",
+    subtitle: "Exposing gender bias women face everyday",
+    author: "Caroline Criado-Perez",
+    Publication_year: {
+        year:2019,
+        month: "June",
+        day: 12,
+}
+
+console.log(invisibleWomen)
+
+//key title
+console.log(invisibleWomen.title)
+//logs Invisible Women
+
+console.log(invisibleWomen.Publication_year)
+
+
+//Defining and using objects
+
+
+//key:value pairs make up objects
+//title:"Invisible Women"
+const invisibleWomen = {
+    title: "Invisible Women",
+    subtitle: "Exposing gender bias women face everyday",
+    author: {
+        author1: "Caroline Criado-Perez",
+        author2: "Jenny Nelson",
+    },//nested object
+    Publication_year: {
+        year:2019,
+        month: "June",
+        day: 12,
+    }
+}
+
+
+console.log(invisibleWomen)
+
+//key title
+console.log(invisibleWomen.title)
+//logs title value, use .key to select
+
+console.log(invisibleWomen.Publication_year.day)
+//prints value of nested key
