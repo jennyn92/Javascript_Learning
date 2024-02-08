76d53158f301ea3777c400deaa55459b5f577957
jennyn92 <134844026+jennyn92@users.noreply.github.com>
2024-02-08 20:55:12 +0000

    functions

---
+++ session3.js
@@ -0,0 +1,12 @@
+//functions
+
+//By using HTML file and adding javascript inside script tags 
+//I can use liveserver to use prompts and see the output on the inspect console
+
+function getSum(num1,nume2){
+    return num1+nume2
+  }
+  
+  const ans =getSum(2,4)
+  console.log(ans)
+  
